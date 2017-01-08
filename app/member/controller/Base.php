e5379e08f8dd268fd1774b524fdbe390a388568b
yykweb <lizhen@youyike.cn>
2017-01-08 16:55:00 +0800

    new

---
+++ app/member/controller/Base.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\member\controller;
+use houdunwang\controller\Controller;
+
+class Base extends Controller
+{
+
+}
---
+++ app/member/controller/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\member\controller;
+/**
+ * Class User 会员中心
+ * @package app\member\controller
+ */
+class User extends Base
+{
+    public function index()
+    {
+        echo 123;
+    }
+}
